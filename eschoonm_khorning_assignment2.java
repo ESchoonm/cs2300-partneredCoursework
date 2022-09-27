d3830480025b6dcc4c1865dd6e80c578691a9680
ESchoonm <98045920+ESchoonm@users.noreply.github.com>
2022-09-27 11:08:32 -0600

    Added a few methods to get us started,
    Added a calculate slope method
    Added the check perpendicular method

--- eschoonm_khorning_assignment2.java
+++ eschoonm_khorning_assignment2.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
 
+/*
+
+*/
+
 public class eschoonm_khorning_assignment2 {
     public static void main(String[] args) {
+
+        ArrayList<Double> slopes; // arraylist for the slopes previously calculated
+
         Board board = new Board(5);
         board.printGameBoard();
         // printBoard(board);
@@ -11,17 +18,52 @@ public class eschoonm_khorning_assignment2 {
         board.printGameBoard();
     }
 
+    /**
+     * Check Perpendicular, checks to see if the play being made is
+     * perpendicular to any previous plays
+     * 
+     * @return
+     */
+    public static Boolean checkPerpendicular(ArrayList<Double> slopes, double calculatedSlope) {
+        // need to implement this
+        return false;
+    }
+
+    /**
+     * This function takes the point read in from the file, and will return the
+     * slope
+     * 
+     * @param sr //starting row
+     * @param sc //strarting column
+     * @param er //ending row
+     * @param ec //ending column
+     * @return
+     */
+    public static double calculateSlope(int sr, int sc, int er, int ec) {
+        // need to implement this
+        return 0;
+    }
+
+    /**
+     * AddSlope function takes the arrayList containing the slopes, and
+     * adds the new slope to the end
+     * 
+     * @param slopes
+     * @param slope
+     */
+    public static void addSlope(ArrayList<Double> slopes, Double slope) {
+        slopes.add(slope); // adds the slope
+    }
+
 }
 
 class Board {
     int[][] gameBoard; // my thoughts we to use and int array with 0 being, initial, 1, "O", 2, "X"
     int size;
-    ArrayList<Double> slopes; // we could always move it outside the board object
 
     Board(int size) {
         gameBoard = new int[size][size];
         this.size = size;
-        slopes = new ArrayList<>();
     }
 
     public void printGameBoard() {
@@ -39,14 +81,6 @@ class Board {
             }
             System.out.println("");
         }
-    }
-
-    public ArrayList<Double> getSlopes() {
-        return slopes;
-    }
-
-    public void addSlope(Double slope) {
-        slopes.add(slope);
     }
 
     public void addMove(int[][] values, int user) {
