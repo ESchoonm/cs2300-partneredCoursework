0da21ba4220d7a1d6198fcefa2a014938d602231
plethorapanther <97998646+plethorapanther@users.noreply.github.com>
2022-10-03 08:22:24 -0600

    Restored moves files

--- eschoonm_khorning_assignment2.java
+++ eschoonm_khorning_assignment2.java
@@ -16,14 +16,14 @@ public class eschoonm_khorning_assignment2 {
         final String FILE_P2_3 = "p2-3.txt";
         final String FILE_P2_4 = "p2-4.txt";
         final String FILE_P2_5 = "p2-5.txt";
-        final String OUTPUT_FILE = "_output";
+        final String OUTPUT_FILE = "output_";
 
         Queue<Double> slopes = new LinkedList<Double>(); // Queue for the slopes previously calculated
         ArrayList<Integer[]> startCells = new ArrayList<>();// ArrayList containing values to check starting values
         ArrayList<Integer[]> endCells = new ArrayList<>();// ArrayList containing values to check ending values
 
         // let the user set the output file name
-        String destinationFile = FILE_P2_1 + OUTPUT_FILE;
+        String destinationFile = OUTPUT_FILE + FILE_P2_1;
         File destFile = new File(destinationFile);
         
 
